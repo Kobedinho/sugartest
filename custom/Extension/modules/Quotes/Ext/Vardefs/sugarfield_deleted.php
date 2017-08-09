a063e7f5aa9eb8488832aa633807ff22613dd078
ruby <rubykariss@gmail.com>
2017-08-09 17:59:46 -0500

    tercera modificacion

---
+++ custom/Extension/modules/Quotes/Ext/Vardefs/sugarfield_deleted.php
@@ -0,0 +1,13 @@
+<?php
+ // created: 2017-08-09 22:57:51
+$dictionary['Quote']['fields']['deleted']['default']=false;
+$dictionary['Quote']['fields']['deleted']['audited']=false;
+$dictionary['Quote']['fields']['deleted']['massupdate']=false;
+$dictionary['Quote']['fields']['deleted']['comments']='Record deletion indicator';
+$dictionary['Quote']['fields']['deleted']['duplicate_merge']='enabled';
+$dictionary['Quote']['fields']['deleted']['duplicate_merge_dom_value']=1;
+$dictionary['Quote']['fields']['deleted']['merge_filter']='disabled';
+$dictionary['Quote']['fields']['deleted']['unified_search']=false;
+$dictionary['Quote']['fields']['deleted']['calculated']=false;
+
+ ?>
